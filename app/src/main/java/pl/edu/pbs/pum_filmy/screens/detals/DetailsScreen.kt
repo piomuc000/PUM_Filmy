ed182037fb607192b1cd18fe0c848aec75a0f38d
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-06-07 22:42:37 +0200

    Poprawka żeby się obrazki z filmów poprawnie wyświetlały

--- app/src/main/java/pl/edu/pbs/pum_filmy/screens/detals/DetailsScreen.kt
+++ app/src/main/java/pl/edu/pbs/pum_filmy/screens/detals/DetailsScreen.kt
@@ -5,10 +5,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -19,10 +20,9 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import coil.compose.rememberImagePainter
+import coil.compose.rememberAsyncImagePainter
 import pl.edu.pbs.pum_filmy.model.Movie
 import pl.edu.pbs.pum_filmy.model.getMovies
 import pl.edu.pbs.pum_filmy.widgets.MovieRow
@@ -67,16 +67,21 @@ fun DetailsScreen(navController: NavController, movieId: Int?) {
 
 @Composable
 private fun HorizontalScrollableImageView(newmovieList: List<Movie>) {
-    LazyRow(modifier = Modifier.height(240.dp)) {
+    LazyRow {
         items(newmovieList[0].images) { image ->
-            val painter: Painter = rememberImagePainter(image)
-            Image(
-                painter = painter,
-                contentDescription = "Movie Poster",
+            Card(
                 modifier = Modifier
                     .padding(12.dp)
-                    .height(200.dp)
-            )
+                    .size(240.dp),
+                elevation = 5.dp
+            ) {
+                val painter = rememberAsyncImagePainter(image)
+                val state = painter.state
+                Image(
+                    painter = painter,
+                    contentDescription = "Movie Poster"
+                )
+            }
         }
     }
 }
